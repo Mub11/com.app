7aa63fecb4185a999c8dab2cd88b7267c532cfd9
Mub11 <m249294716@163.com>
2019-11-11 20:01:40 +0800

    2019.11.11

---
+++ Frame/src/loginform/LoginForm.java
@@ -0,0 +1,49 @@
+package loginform;
+
+import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
+
+import javax.swing.*;
+
+public class LoginForm extends JFrame {
+    private JPanel panelLogin;
+    private JLabel userLabel;
+    private JTextField userText;
+    private JLabel passLabel;
+    private JPasswordField passText;
+    private JButton loginButton;
+    private JButton registerButton;
+    private JButton helpButton;
+    public LoginForm(){
+        setTitle("Login Form");
+        setSize(350,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panelLogin = new JPanel();
+        panelLogin.setLayout(null);
+        userLabel = new JLabel("Username:");
+        userLabel.setBounds(10,20,80,25);
+        panelLogin.add(userLabel);
+        passLabel = new JLabel("Password:");
+        passLabel.setBounds(10,50,80,25);
+        panelLogin.add(passLabel);
+        userText = new JTextField(10);
+        userText.setBounds(100,20,170,25);
+        panelLogin.add(userText);
+        passText = new JPasswordField(10);
+        passText.setBounds(100,50,170,25);
+        panelLogin.add(passText);
+        loginButton = new JButton("login");
+        loginButton.setBounds(10,80,80,25);
+        panelLogin.add(loginButton);
+        registerButton = new JButton("register");
+        registerButton.setBounds(100,80,80,25);
+        panelLogin.add(registerButton);
+        helpButton = new JButton("help");
+        helpButton.setBounds(190,80,80,25);
+        panelLogin.add(helpButton);
+        add(panelLogin);
+    }
+    public void setFrameLoginVisibe(Boolean Visibe){
+        setVisible(true);
+    }
+}
---
+++ Frame/src/loginform/QQForm.java
@@ -0,0 +1,8 @@
+package loginform;
+
+public class QQForm {
+    public static void main(String[] args){
+        LoginForm loginForm = new LoginForm();
+        loginForm.setFrameLoginVisibe(true);
+    }
+}
